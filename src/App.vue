45ad9b4615f7e7c278a47b24b35039c875b90dce
Froconic <froconicdesign@gmail.com>
2021-03-26 13:53:25 -0500

    timer made reactions tracked

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>My Reaction Timer</h1>
+  <button @click="start" :disable="playing">Play Game</button>
+  <Block v-if="playing" :delay="delay" @end="gameOver"></Block>
+  <p v-if="showResults">You scored: {{ score }} sec</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  components: { Block },
+
+  data() {
+
+    return {
+      playing: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.playing = true
+      this.showResults = false
+
+          },
+    gameOver(reactionTime) {
+      this.score = reactionTime / 1000
+      this.playing = false
+      this.showResults = true
+    },
   }
 }
 </script>
@@ -20,7 +46,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #5d87b1;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">Hit me</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false,
+        timer: null,
+        reactionTime: 0,
+      }
+    },
+    // Lifestyle hooks
+    // A hook to fire when the component is mounted
+    mounted() {
+      setTimeout(() => {
+        this.showBlock = true
+        this.startTimer()
+      }, this.delay)
+    },
+    methods: {
+      startTimer() {
+        this.timer = setInterval(() => {
+          this.reactionTime += 10
+        }, 10)
+      },
+
+      stopTimer() {
+        clearInterval(this.timer)
+        this.$emit('end', this.reactionTime)
+      }
+    }
+}
+</script>
+
+<style>
+.block {
+    width: 500px;
+    height: 500px;
+    border-radius: 50px;
+    background: rgb(12, 189, 150);
+    color: #fff;
+    text-align: center;
+    padding: 20px 0;
+    margin: 20px auto;
+}
+</style>
